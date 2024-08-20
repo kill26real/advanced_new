de26bbd741e5e8094696155321a029ca5275aef0
Kirill Safelkin <k.safelkin@mail.ru>
2024-08-20 11:34:25 +0200

    1 module done

--- module_01_flask/homework/app.py
+++ module_01_flask/homework/app.py
@@ -1,42 +1,65 @@
-import datetime
+from datetime import datetime, timedelta
 from flask import Flask
+import random
+import os
+import re
 
 app = Flask(__name__)
 
 
+CARS = 'Chevrolet, Renault, Ford, Lada'
+
+CATS = ( 'корниш-рекс', 'русская голубая', 'шотландская вислоухая', 'мейн-кун', 'манчкин')
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+BOOK_FILE = os.path.join(BASE_DIR, 'war_and_peace.txt')
+
+with open(BOOK_FILE, 'r', encoding='utf-8') as book:
+    text = book.read()
+    pattern = r'\b\w+\b'
+    WORDS = re.findall(pattern, text)
+
+counter = 0
+
+
 @app.route('/hello_world')
-def test_function():
-    pass
+def hello():
+    return 'Hallo, World!'
 
 
 @app.route('/cars')
-def test_function():
-    pass
+def cars():
+    return CARS
 
 
 @app.route('/cats')
-def test_function():
-    pass
+def cats():
+    return random.choice(CATS)
 
 
 @app.route('/get_time/now')
-def test_function():
-    pass
+def time_now():
+    current_time = datetime.now()
+    return f'Точное время: {current_time.strftime("%H:%M:%S")}'
 
 
 @app.route('/get_time/future')
-def test_function():
-    pass
+def time_one_hour():
+    current_time = datetime.now()
+    current_time_after_hour = current_time + timedelta(hours=1)
+    return f'Точное время: {current_time.strftime("%H:%M:%S")} \nТочное время через час будет {current_time_after_hour.strftime("%H:%M:%S")}'
 
 
 @app.route('/get_random_word')
-def test_function():
-    pass
+def random_word():
+    return random.choice(WORDS)
 
 
 @app.route('/counter')
-def test_function():
-    pass
+def counter_visits():
+    global counter
+    counter += 1
+    return str(counter)
 
 
 if __name__ == '__main__':
--- module_01_flask/materials/first_application/app.py
+++ module_01_flask/materials/first_application/app.py
@@ -4,7 +4,7 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/test')
+@app.route('/testtt')
 def test_function():
     now = datetime.datetime.now().utcnow()
-    return f'Это тестовая страничка, ответ сгенерирован в {now}'
+    return f'Это тестовая страничка, ответ сгенерирован aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa в {now}'
