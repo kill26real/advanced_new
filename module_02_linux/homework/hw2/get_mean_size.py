1b3d4e0d4dd24ed90354abec5c82df692fef2980
kill_26_real <k.safelkin@mail.ru>
2024-10-09 14:14:54 +0200

    all ist new

--- module_02_linux/homework/hw2/get_mean_size.py
+++ module_02_linux/homework/hw2/get_mean_size.py
@@ -9,9 +9,28 @@ $ ls -l | python3 get_mean_size.py
 
 import sys
 
+def convert_bytes(num_bytes):
+    if num_bytes < 1024:
+        return f"{num_bytes} Bytes"
+    elif num_bytes < 1024**2:
+        return f"{num_bytes / 1024:.2f} KB"
+    elif num_bytes < 1024**3:
+        return f"{num_bytes / 1024**2:.2f} MB"
+    elif num_bytes < 1024**4:
+        return f"{num_bytes / 1024**3:.2f} GB"
+    else:
+        return f"{num_bytes / 1024**4:.2f} TB"
 
-def get_mean_size(ls_output: str) -> float:
-    ...
+def get_mean_size(data: str) -> float:
+    answer = ""
+
+    with open(data, 'r', encoding="UTF-8") as file:
+        lines = file.readlines()[2:-2]
+        for line in lines:
+            columns = line.split()
+            format_bytes = convert_bytes(int(columns[3]))
+            answer += f"Process {columns[-1]} use {format_bytes}\n"
+    return answer
 
 
 if __name__ == '__main__':
